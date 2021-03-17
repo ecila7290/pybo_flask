471b331051f5619de4578c5b1788f01b6fec6f4a
jeehoon kim <jihoon.kim89@gmail.com>
2021-03-17 22:13:11 +0900

    Add: added question list and detail views

--- views/main_views.py
+++ views/main_views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for
+from werkzeug.utils import redirect
 
 bp=Blueprint('main', __name__, url_prefix='/')
 
@@ -8,4 +9,5 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    return 'pybo index'
+    return redirect(url_for('question._list'))
+    
